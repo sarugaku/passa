c4acf33bff0d107a4ef269cc72d3a4c232714128
Dan Ryan <dan@danryan.co>
2018-08-08 00:23:45 -0400

    Add options to the resolver
    - Clean up some old errors
    
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/passa/cli.py
+++ src/passa/cli.py
@@ -15,8 +15,8 @@ import json
 import os
 
 from requirementslib import Pipfile, Requirement
-from requirementslib.models.cache import HashCache
-from requirementslib.utils import temp_cd
+from requirementslib.models.cache import HashCache, CACHE_DIR
+from requirementslib.utils import temp_cd, temp_environ, fs_str
 from resolvelib import NoVersionsAvailable, ResolutionImpossible
 
 from .lockfile import build_lockfile, trace
@@ -27,12 +27,61 @@ from .resolver import Resolver
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
+    parser.add_argument("--project", type=os.path.abspath)
+    parser.add_argument("--write", action="store_true", default=False)
     parser.add_argument(
-        'requirements', metavar='REQUIREMENT',
-        nargs='*', type=Requirement.from_line,
+        "--ignore-hashes", action="store_true", default=False, help="Ignore hashes"
     )
-    parser.add_argument('--project', type=os.path.abspath)
-    parser.add_argument('--write', action='store_true', default=False)
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="Print verbose logging to the console",
+    )
+    parser.add_argument(
+        "-i", "--index", type=str, nargs="?", help="Index to search for packages"
+    )
+    parser.add_argument(
+        "--cache-dir",
+        type=str,
+        nargs="?",
+        default=CACHE_DIR,
+        help="Cache directory to use",
+    )
+    parser.add_argument(
+        "--extra-index",
+        "--extra-index-url",
+        type=str,
+        action="append",
+        help="Extra indexes to search",
+    )
+    parser.add_argument(
+        "--trusted-host",
+        type=str,
+        action="append",
+        help="Addresses where we will skip ssl verification",
+    )
+    parser.add_argument(
+        "--selective-upgrade",
+        action="store_true",
+        default=False,
+        help="Perform a selective upgrade.",
+    )
+    parser.add_argument(
+        "--src",
+        "--src-dir",
+        nargs="?",
+        type=str,
+        help="Location to check out repositories",
+    )
+    parser.add_argument(
+        "requirements",
+        action="append",
+        nargs="*",
+        metavar="REQUIREMENT",
+        type=Requirement.from_line,
+        help="Packages to install",
+    )
     return parser.parse_args(argv)
 
 
@@ -45,19 +94,19 @@ def resolve(requirements, pipfile=None, write=False):
     try:
         state = r.resolve(requirements)
     except NoVersionsAvailable as e:
-        print('\nCANNOT RESOLVE. NO CANDIDATES FOUND FOR:')
-        print('{:>40}'.format(e.requirement.as_line(include_hashes=False)))
+        print("\nCANNOT RESOLVE. NO CANDIDATES FOUND FOR:")
+        print("{:>40}".format(e.requirement.as_line(include_hashes=False)))
         if e.parent:
             line = e.parent.as_line(include_hashes=False)
-            print('{:>41}'.format('(from {})'.format(line)))
+            print("{:>41}".format("(from {})".format(line)))
         else:
-            print('{:>41}'.format('(user)'))
+            print("{:>41}".format("(user)"))
     except ResolutionImpossible as e:
-        print('\nCANNOT RESOLVE.\nOFFENDING REQUIREMENTS:')
+        print("\nCANNOT RESOLVE.\nOFFENDING REQUIREMENTS:")
         for r in e.requirements:
             print_requirement(r)
     else:
-        print_title(' STABLE PINS ')
+        print_title(" STABLE PINS ")
         lockfile = build_lockfile(r, state, hash_cache, pipfile=pipfile)
         criteria = r.criteria
         reverse_deps = trace(state.graph)
@@ -66,18 +115,18 @@ def resolve(requirements, pipfile=None, write=False):
             paths = reverse_deps[k]
             if paths:
                 for path in paths:
-                    print('   ', end='')
+                    print("   ", end="")
                     for v in reversed(path):
                         line = state.mapping[v].as_line(include_hashes=False)
-                        print(' <=', line, end='')
+                        print(" <=", line, end="")
                     print()
             else:
-                print('    User requirement')
+                print("    User requirement")
 
         if write:
             lockfile.write()
         else:
-            print_title(' LOCK FILE ')
+            print_title(" LOCK FILE ")
             print(json.dumps(lockfile.as_dict(), indent=4))
 
 
@@ -85,11 +134,45 @@ def cli(argv=None):
     options = parse_arguments(argv)
     requirements = list(options.requirements)
     pipfile = None
-    with temp_cd(options.project or os.getcwd()):
+    with temp_environ(), temp_cd(options.project or os.getcwd()):
+        extra_indexes, trusted_hosts, sources = [], [], []
         if options.project:
             pipfile = Pipfile.load(options.project)
+            sources = [s.expanded.url for s in pipfile.sources]
+            trusted_hosts = [s.url for s in pipfile.sources if not s.verify_ssl]
             requirements.extend(pipfile.dev_packages.requirements)
             requirements.extend(pipfile.packages.requirements)
+        if options.index:
+            sources.append(options.index)
+        if options.extra_index:
+            sources.extend(options.extra_index)
+        if options.trusted_hosts:
+            trusted_hosts.extend(options.trusted_hosts)
+        for i, source in enumerate(sources):
+            if i == 1:
+                os.environ["PIP_INDEX"] = fs_str(source)
+            else:
+                extra_indexes.append(source)
+        if trusted_hosts:
+            os.environ["PIP_TRUSTED_HOST"] = fs_str(" ".join(trusted_hosts))
+        if extra_indexes:
+            os.environ["PIP_EXTRA_INDEX_URL"] = fs_str(" ".join(extra_indexes))
+        if options.cache_dir:
+            os.environ["PIP_CACHE_DIR"] = fs_str(options.cache_dir)
+            os.environ["PIP_WHEEL_DIR"] = fs_str("{0}/wheels".format(options.cache_dir))
+            os.environ["PIP_DESTINATION_DIR"] = fs_str(
+                "{0}/pkgs".format(options.cache_dir)
+            )
+        if "PIP_SRC" not in os.environ and options.src:
+            os.environ["PIP_SRC"] = fs_str(options.src)
+        if not options.ignore_hashes:
+            os.environ["PIP_REQUIRE_HASHES"] = fs_str("1")
+        if options.selective_upgrade:
+            os.environ["PIP_UPGRADE"] = fs_str("1")
+            os.environ["PIP_UPGRADE_STRATEGY"] = fs_str("only-if-needed")
+            os.environ["PIP_EXISTS_ACTION"] = fs_str("w")
+        else:
+            os.environ["PIP_EXISTS_ACTION"] = fs_str("i")
         resolve(requirements, pipfile=pipfile, write=options.write)
 
 
