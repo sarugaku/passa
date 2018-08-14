cfc18ff770ed06a87906395638380405d2039893
Tzu-ping Chung <uranusjr@gmail.com>
2018-08-14 18:07:41 +0800

    Integrate marker manipulation into locking
    Also extract hash fetching into its own module because I like smalle
    modules.

--- src/passa/locking.py
+++ src/passa/locking.py
@@ -1,119 +1,17 @@
-import contextlib
-import copy
 import itertools
 
-from pip_shims import Wheel
+
 from plette import Lockfile
 from requirementslib import Requirement
 from resolvelib import Resolver
 
 from .caches import HashCache
+from .hashes import get_hashes
+from .markers import set_markers
 from .providers import RequirementsLibProvider
 from .reporters import StdOutReporter
 from .traces import trace_graph
 from .utils import identify_requirment
-
-
-def _wheel_supported(self, tags=None):
-    # Ignore current platform. Support everything.
-    return True
-
-
-def _wheel_support_index_min(self, tags=None):
-    # All wheels are equal priority for sorting.
-    return 0
-
-
-@contextlib.contextmanager
-def _allow_all_wheels():
-    """Monkey patch pip.Wheel to allow all wheels
-
-    The usual checks against platforms and Python versions are ignored to allow
-    fetching all available entries in PyPI. This also saves the candidate cache
-    and set a new one, or else the results from the previous non-patched calls
-    will interfere.
-    """
-    original_wheel_supported = Wheel.supported
-    original_support_index_min = Wheel.support_index_min
-
-    Wheel.supported = _wheel_supported
-    Wheel.support_index_min = _wheel_support_index_min
-    yield
-    Wheel.supported = original_wheel_supported
-    Wheel.support_index_min = original_support_index_min
-
-
-def _get_hashes(cache, req):
-    ireq = req.as_ireq()
-    if req.is_vcs or ireq.editable or not ireq.is_pinned:
-        return set()
-
-    matching_candidates = set()
-    with _allow_all_wheels():
-        matching_candidates = req.find_all_matches()
-
-    return {
-        cache.get_hash(candidate.location)
-        for candidate in matching_candidates
-    }
-
-
-def _markerset(*markers):
-    return frozenset(markers)
-
-
-def _add_markersets(specs, key, trace, all_markersets):
-    markersets = set()
-    for route in trace:
-        parent = route[-1]
-        try:
-            parent_markersets = all_markersets[parent]
-        except KeyError:    # Parent not calculated yet. Wait for it.
-            return False
-        r = specs[parent][key]
-        if r.markers:
-            markerset = _markerset(r.markers)
-        else:
-            markerset = _markerset()
-        markersets.update({
-            parent_markerset | markerset
-            for parent_markerset in parent_markersets
-        })
-    try:
-        current_markersets = all_markersets[key]
-    except KeyError:
-        all_markersets[key] = markersets
-    else:
-        all_markersets[key] = current_markersets | markersets
-    return True
-
-
-def _calculate_markersets_mapping(specs, requirements, state, traces):
-    all_markersets = {}
-
-    # Populate markers from Pipfile.
-    for r in requirements:
-        if r.markers:
-            markerset = _markerset(r.markers)
-        else:
-            markerset = _markerset()
-        all_markersets[identify_requirment(r)] = {markerset}
-
-    traces = copy.deepcopy(traces)
-    del traces[None]
-    while traces:
-        successful_keys = set()
-        for key, trace in traces.items():
-            ok = _add_markersets(specs, key, trace, all_markersets)
-            if not ok:
-                continue
-            successful_keys.add(key)
-        if not successful_keys:
-            break   # No progress? Deadlocked. Give up.
-        for key in successful_keys:
-            del traces[key]
-
-    return all_markersets
 
 
 def resolve_requirements(requirements):
@@ -137,27 +35,32 @@ def resolve_requirements(requirements):
 
     hash_cache = HashCache()
     for r in state.mapping.values():
-        r.hashes = _get_hashes(hash_cache, r)
+        r.hashes = get_hashes(hash_cache, r)
 
-    markersets_mapping = _calculate_markersets_mapping(
-        provider.fetched_dependencies, requirements, state, traces,
+    set_markers(
+        state.mapping, traces,
+        requirements, provider.fetched_dependencies,
     )
-    print('RESULT', markersets_mapping)
-    # for k, r in state.mapping.items():
-    #     markers = [
-    #         " and ".join("({0})".format(p) for p in route if p)
-    #         for routes in marker_routes[k]
-    #         for route in routes
-    #     ]
-    #     if any(not m for m in markers):
-    #         # Needs to be unconditional if there is an unconditional route.
-    #         pass
-    #     r.markers =
-
     return state, traces
 
 
-def _get_derived_requirement_data(state, traces, names):
+def _get_requirements(pipfile, section_name):
+    """Produce a mapping of identifier: requirement from the section.
+    """
+    return {identify_requirment(r): r for r in (
+        Requirement.from_pipfile(name, package._data)
+        for name, package in pipfile.get(section_name, {}).items()
+    )}
+
+
+def _get_derived_entries(state, traces, names):
+    """Produce a mapping containing all candidates derived from `names`.
+
+    `name` should provide a collection of requirement identifications from
+    a section (i.e. `packages` or `dev-packages`). This function uses `trace`
+    to filter out candidates in the state that are present because of an entry
+    in that collection.
+    """
     if not names:
         return {}
     return {
@@ -168,31 +71,20 @@ def _get_derived_requirement_data(state, traces, names):
 
 
 def build_lockfile(pipfile):
-    default_reqs = [
-        Requirement.from_pipfile(name, package._data)
-        for name, package in pipfile.get("packages", {}).items()
-    ]
-    develop_reqs = [
-        Requirement.from_pipfile(name, package._data)
-        for name, package in pipfile.get("dev-packages", {}).items()
-    ]
+    default_reqs = _get_requirements(pipfile, "packages")
+    develop_reqs = _get_requirements(pipfile, "dev-packages")
 
     # This comprehension dance ensures we merge packages from both
     # sections, and definitions in the default section win.
-    requirements = {
-        identify_requirment(r): r
-        for r in itertools.chain(develop_reqs, default_reqs)
-    }.values()
+    requirements = {k: r for k, r in itertools.chain(
+        develop_reqs.items(), default_reqs.items(),
+    )}.values()
 
     state, traces = resolve_requirements(requirements)
 
     lockfile = Lockfile.with_meta_from(pipfile)
-    lockfile["default"] = _get_derived_requirement_data(
-        state, traces, set(identify_requirment(r) for r in default_reqs),
-    )
-    lockfile["develop"] = _get_derived_requirement_data(
-        state, traces, set(identify_requirment(r) for r in develop_reqs),
-    )
+    lockfile["default"] = _get_derived_entries(state, traces, default_reqs)
+    lockfile["develop"] = _get_derived_entries(state, traces, develop_reqs)
     return lockfile
 
 
