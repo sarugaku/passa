7f6085b507258492359a95e5b462b0229eef037c
Dan Ryan <dan@danryan.co>
2018-08-18 01:32:46 -0400

    Use new method for copying requirement
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/passa/candidates.py
+++ src/passa/candidates.py
@@ -34,8 +34,7 @@ def _filter_matching_python_requirement(candidates, python_version):
 
 
 def _copy_requirement(requirement):
-    name, data = next(iter(requirement.as_pipfile().items()))
-    return requirementslib.Requirement.from_pipfile(name, data)
+    return requirement.copy()
 
 
 def _requirement_from_metadata(name, version, extras, index):
