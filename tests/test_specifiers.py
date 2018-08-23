6cb6e1c84750e98e85e6d60148b230d84c040434
Tzu-ping Chung <uranusjr@gmail.com>
2018-08-23 16:23:18 +0800

    Add comments to explain test cases

--- tests/test_specifiers.py
+++ tests/test_specifiers.py
@@ -6,16 +6,25 @@ from passa.markers import cleanup_specs
 
 
 @pytest.mark.parametrize("spec, cleaned", [
-    (">=3.0", {">=3.0"}),
-    (">3.0", {">=3.1"}),
-    ("~=3.0", {"~=3.0"}),
-    # <=3.3 should convert to <3.4
-    (">=2.7,<=3.3", {">=2.7", "<3.4"}),
-    # >2.6 should convert to >=2.7
-    (">2.6,!=3.0,!=3.1,!=3.2", {">=2.7", "!=3.0", "!=3.1", "!=3.2"}),
-    (">2.6,>=2.7,<=3.3,<3.4,!=3.0,!=3.1,!=3.2", {">=2.7", "<3.4", "!=3.0", "!=3.1", "!=3.2"}),
+    # == and ~= don't matter.
+    ("~=3.0", "~=3.0"),
+    ("==2.7", "==2.7"),
+
+    # >= and < should be kept.
+    (">=3.0", ">=3.0"),
+    ("<3.5", "<3.5"),
+
+    # > and <= should be converted to >= and <.
+    (">3.0", ">=3.1"),
+    (">=2.7,<=3.3", ">=2.7,<3.4"),
+    (">2.6,!=3.0,!=3.1,!=3.2", ">=2.7,!=3.0,!=3.1,!=3.2"),
+
+    # The result should be dedup-ed.
+    (
+        ">2.6,>=2.7,<=3.3,<3.4,!=3.0,!=3.1,!=3.2",
+        ">=2.7,<3.4,!=3.0,!=3.1,!=3.2",
+    ),
 ])
 def test_cleanup_specs(spec, cleaned):
-    # cleaned_specifierset = SpecifierSet(",".join(list(cleaned)))
-    cleaned_specifierset = frozenset({Specifier(s) for s in cleaned})
+    cleaned_specifierset = frozenset(Specifier(s) for s in cleaned.split(","))
     assert cleanup_specs(SpecifierSet(spec)) == cleaned_specifierset
