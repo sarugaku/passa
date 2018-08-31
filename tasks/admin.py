b1ebc988a001b9bc8b5d0d941b547fd5aef3331f
Tzu-ping Chung <uranusjr@gmail.com>
2018-08-31 09:04:00 +0800

    Add task to undo prebump

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,7 @@
 import ast
 import pathlib
 import shutil
+import subprocess
 
 import invoke
 import parver
@@ -48,11 +49,35 @@ REL_TYPES = ('major', 'minor', 'patch',)
 
 
 @invoke.task()
+def unprebump(ctx):
+    out = subprocess.check_output(['git', 'tag'], encoding='ascii')
+    try:
+        version = max(parver.Version.parse(v).normalize() for v in (
+            line.strip() for line in out.split('\n')
+        ) if v)
+    except ValueError:
+        print('[unprebump] No tag to undo prebump')
+        return
+    print(f'[unprebump] Undo prebump back to {version}')
+    _write_version(version)
+
+
+@invoke.task()
 def bump_release(ctx, type_):
     if type_ not in REL_TYPES:
         raise ValueError(f'{type_} not in {REL_TYPES}')
     index = REL_TYPES.index(type_)
     prev_version = _read_version()
+
+    if prev_version.is_prerelease:
+        print(f'[bump] Bumping {type_} from prerelease ({prev_version}).')
+        print('[bump] Did you forget to undo prebumping?')
+        try:
+            input('[bump] ENTER to continue, CTRL-C to abort: ')
+        except KeyboardInterrupt:
+            print('\nAborted!')
+            return 1
+
     next_version = prev_version.base_version().bump_release(index)
     print(f'[bump] {prev_version} -> {next_version}')
     _write_version(next_version)
@@ -68,12 +93,13 @@ def upload(ctx, repo):
     dist_pattern = f'{PACKAGE_NAME.replace("-", "[-_]")}-*'
     artifacts = list(ROOT.joinpath('dist').glob(dist_pattern))
     filename_display = '\n'.join(f'  {a}' for a in artifacts)
+
     print(f'[release] Will upload:\n{filename_display}')
     try:
         input('[release] Release ready. ENTER to upload, CTRL-C to abort: ')
     except KeyboardInterrupt:
         print('\nAborted!')
-        return
+        return 1
 
     arg_display = ' '.join(f'"{n}"' for n in artifacts)
     ctx.run(f'twine upload --repository="{repo}" {arg_display}')
@@ -97,7 +123,9 @@ PREBUMP = 'patch'
 def release(ctx, type_, repo=None, prebump_to=PREBUMP):
     """Make a new release.
     """
-    bump_release(ctx, type_=type_)
+    unprebump(ctx)
+    if bump_release(ctx, type_=type_):
+        return
 
     this_version = _read_version()
     ctx.run('towncrier')
@@ -105,7 +133,8 @@ def release(ctx, type_, repo=None, prebump_to=PREBUMP):
     ctx.run(f'git tag -fa {this_version} -m "Version {this_version}"')
 
     if repo:
-        upload(ctx, repo=repo)
+        if upload(ctx, repo=repo):
+            return
     else:
         print('[release] Missing --repo, skip uploading')
 
