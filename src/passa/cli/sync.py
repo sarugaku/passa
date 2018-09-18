4186a0ce7dbfacfd11094c54808c01d76f16cfe7
Dan Ryan <dan@danryan.co>
2018-09-18 05:23:23 -0400

    Clean up options and add virtualenv module
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/passa/cli/sync.py
+++ src/passa/cli/sync.py
@@ -4,14 +4,14 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from ..actions.sync import sync
 from ._base import BaseCommand
-from .options import dev, no_clean
+from .options import dev, clean_group
 
 
 class Command(BaseCommand):
 
     name = "sync"
     description = "Install Pipfile.lock into the environment."
-    arguments = [dev, no_clean]
+    arguments = [dev, clean_group]
 
     def run(self, options):
         return sync(project=options.project, dev=options.dev, clean=options.clean)
