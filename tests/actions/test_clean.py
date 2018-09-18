ad79981b26516596cdb15ef2f2e8cefd2f7035ef
Dan Ryan <dan@danryan.co>
2018-09-18 05:23:46 -0400

    Add tests (clean is still failing)
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- tests/actions/test_clean.py
+++ tests/actions/test_clean.py
@@ -1,18 +1,18 @@
 # -*- coding=utf-8 -*-
-from passa.actions.add import add_packages
-from passa.cli.options import Project
-import vistir
 
 
-def test_clean_subset(project_directory):
-    with vistir.contextmanagers.cd(project_directory.strpath):
-        project = Project(project_directory.strpath)
-        retcode = add_packages(["requests"], project=project)
-        assert not retcode
-        packages = ["requests", "chardet", "certifi", "idna"]
-        assert all(pkg in project.lockfile.default for pkg in packages)
-        import passa.actions.clean
-        pass
-        # clean_retcode = passa.actions.clean.clean(project=project)
-        # assert not clean_retcode
-        # assert project.lockfile.default._data == {}
+def test_clean_subset(project):
+    from passa.actions.add import add_packages
+    from passa.actions.clean import clean
+    retcode = add_packages(["requests"], project=project)
+    assert not retcode
+    packages = ["requests", "chardet", "certifi", "idna"]
+    c = project.venv.run("pip install pytz")
+    assert c.returncode == 0
+    c = project.venv.run("python -c 'import pytz'")
+    assert c.returncode == 0
+    clean_retcode = clean(project=project)
+    assert not clean_retcode
+    c = project.venv.run("python -c 'import pytz'")
+    assert c.returncode != 0
+    assert all(pkg in project.lockfile.default for pkg in packages)
