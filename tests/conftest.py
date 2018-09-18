89325f50b4186d8757192b5ef5c960754920e4b2
Dan Ryan <dan@danryan.co>
2018-09-18 05:23:46 -0400

    Add tests (clean is still failing)
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
 # -*- coding=utf-8 -*-
+import os
 import pytest
+import passa.cli.options
+import passa.models.virtualenv
+import sys
+import vistir
 
 
 DEFAULT_PIPFILE_CONTENTS = """
@@ -18,4 +23,35 @@ verify_ssl = true
 def project_directory(tmpdir_factory):
     project_dir = tmpdir_factory.mktemp("passa-project")
     project_dir.join("Pipfile").write(DEFAULT_PIPFILE_CONTENTS)
-    return project_dir
+    with vistir.contextmanagers.cd(project_dir.strpath):
+        yield project_dir
+
+
+@pytest.fixture(scope="function")
+def virtualenv(tmpdir_factory):
+    venv_dir = tmpdir_factory.mktemp("passa-testenv")
+    print("Creating virtualenv {0!r}".format(venv_dir.strpath))
+    c = vistir.misc.run([sys.executable, "-m", "virtualenv", venv_dir.strpath],
+                            return_object=True, block=True, nospin=True)
+    if c.returncode == 0:
+        print("Virtualenv created...")
+        return venv_dir
+    raise RuntimeError("Failed creating virtualenv for testing...{0!r}".format(c.err.strip()))
+
+
+class _Project(passa.cli.options.Project):
+    def __init__(self, root, venv=None):
+        self.path = os.path.abspath(root)
+        self.venv = venv
+        super(_Project, self).__init__(self.path)
+
+
+@pytest.fixture
+def tmpvenv(virtualenv):
+    return passa.models.virtualenv.VirtualEnv(virtualenv)
+
+
+@pytest.fixture(scope="function")
+def project(project_directory, tmpvenv):
+    with tmpvenv.activated():
+        yield _Project(project_directory, tmpvenv)
