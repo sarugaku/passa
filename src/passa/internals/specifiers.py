0ffc28ebec99d6b7968b707ed3cfaa0d52f40e2a
Dan Ryan <dan@danryan.co>
2018-09-22 19:09:00 -0400

    Add PySpec object to handle comparison and consolidation
    - Also add magic methods to MetaSet object
    - Super fast and efficient now due to set comparisons
    - Old method had sometimes thousands of the same pyspecs
    - This consolidates as it builds
    
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/passa/internals/specifiers.py
+++ src/passa/internals/specifiers.py
@@ -2,9 +2,11 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import collections
 import itertools
 import operator
 
+from cached_property import cached_property
 from packaging.markers import Marker
 from packaging.specifiers import Specifier, SpecifierSet
 
@@ -44,7 +46,6 @@ def _format_pyspec(specifier):
         op = REPLACE_RANGES[specifier.operator]
     except KeyError:
         return specifier
-    version = specifier.version.replace(".*", "")
     curr_tuple = _tuplize_version(version)
     try:
         next_tuple = (curr_tuple[0], curr_tuple[1] + 1)
@@ -152,3 +153,163 @@ def pyspec_from_markers(marker):
     if specset:
         return specset
     return None
+
+
+class PySpecs(collections.Set):
+    def __init__(self, specs=None):
+        if not specs:
+            specs = SpecifierSet()
+        self.specifierset = specs
+        self.previous_specifierset = None
+        self.cleaned_tuples = set()
+        self.markers = None
+        self.clean()
+
+    def __key(self):
+        return tuple(sorted(self.specifierset, key=operator.attrgetter("_spec")))
+
+    def __contains__(self, other):
+        # The current specifierset fully has every value in the supplied specifierset
+        if not other.as_set - self.as_set:
+            return True
+        return False
+
+    def __eq__(self, other):
+        return self.__key() == other.__key()
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __repr__(self):
+        return u"PySpecs({0!r})".format(str(self.specifierset))
+
+    def __len__(self):
+        return len(self.cleaned_tuples)
+
+    def __iter__(self):
+        for version in self.as_string_set:
+            yield version
+        return
+
+    def clean(self):
+        if len(set(self.specifierset)) == 1:
+            spec = next(iter(spec for spec in self.specifierset), None)
+            if spec:
+                self.cleaned_tuples.add((spec.operator, spec.version))
+        else:
+            self.cleaned_tuples = cleanup_pyspecs(self.specifierset)
+            self.specifierset = self.as_specset
+
+    def add(self, other):
+        new_pyspec = PySpecs(self.specifierset)
+        new_pyspec.specifierset &= other.specifierset
+        return new_pyspec
+
+    @cached_property
+    @lru_cache(maxsize=128)
+    def as_specset(self):
+        specs = set()
+        for spec in self.cleaned_tuples:
+            op, value = spec
+            if op in ('in', 'not in'):
+                new_op = '!=' if op == 'not in' else '=='
+                for val in value.split(","):
+                    specs.add(Specifier("{0}{1}".format(new_op, val)))
+            else:
+                specs.add(Specifier("{0}{1}".format(op, value)))
+        specifierset = SpecifierSet()
+        specifierset._specs = frozenset(specs)
+        return specifierset
+
+    @cached_property
+    @lru_cache(maxsize=128)
+    def as_set(self):
+        return set(self.specifierset)
+
+    @cached_property
+    @lru_cache(maxsize=128)
+    def as_string_set(self):
+        returnval = set()
+        if len(self.cleaned_tuples) == 1:
+            val = next(iter(spec for spec in self.cleaned_tuples), None)
+            if val:
+                returnval.add("python_version {0[0]} '{0[1]}'".format(val))
+            return returnval
+        return set(
+            "python_version {0[0]} '{0[1]}'".format(s)
+            for s in sorted(self.cleaned_tuples)
+        )
+
+    @cached_property
+    @lru_cache(maxsize=128)
+    def marker_set(self):
+        markerset = {Marker(spec) for spec in self.as_string_set}
+        return markerset
+
+    @cached_property
+    @lru_cache(maxsize=128)
+    def marker_string(self):
+        marker_string = " and ".join(sorted(str(m) for m in self.as_string_set))
+        if not marker_string:
+            return ""
+        return marker_string
+
+    @cached_property
+    @lru_cache(maxsize=128)
+    def as_markers(self):
+        print("generating marker using string: %s" % self.marker_string)
+        if not self.marker_string:
+            return ""
+        marker = Marker(self.marker_string)
+        return marker
+
+    @lru_cache(maxsize=128)
+    def __str__(self):
+        string_repr = u"{0}".format(str(self.marker_string))
+        print("converting to string: %s" % string_repr)
+        return string_repr
+
+    def __bool__(self):
+        return bool(self.specifierset)
+
+    def __nonzero__(self):  # Python 2.
+        return self.__bool__()
+
+    @lru_cache(maxsize=128)
+    def __or__(self, specset):
+        if not isinstance(specset, PySpecs):
+            specset = PySpecs(specset)
+        if str(self) == str(specset):
+            return self
+        combined_set = self.as_set | specset.as_set
+        new_specset = SpecifierSet()
+        new_specset._specs = frozenset(combined_set)
+        new_pyspec = PySpecs(new_specset)
+        return new_pyspec
+
+    @classmethod
+    @lru_cache(maxsize=128)
+    def from_marker(cls, marker):
+        if marker._markers[0][0] != 'python_version':
+            return
+        op = marker._markers[0][1].value
+        version = marker._markers[0][2].value
+        specset = set()
+        if op == "in":
+            specset.update(
+                Specifier("=={0}".format(v.strip()))
+                for v in version.split(",")
+            )
+        elif op == "not in":
+            specset.update(
+                Specifier("!={0}".format(v.strip()))
+                for v in version.split(",")
+            )
+        else:
+            specset.add(Specifier("".join([op, version])))
+        if specset:
+            specifierset = SpecifierSet()
+            specifierset._specs = frozenset(specset)
+            newset = cls(specifierset)
+            return newset
+        return None
